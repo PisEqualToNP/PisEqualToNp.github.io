f2ff64c36b6dee9515d2fa89f79fc0a9a54a15b4
theblackathena <nicholaschin20@gmail.com>
2019-01-27 00:58:19 -0800

    jsdkf

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const port = 5000;
 const db = mysql.createConnection ({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: 'nickjon20',
     database: 'debtDB'
 });
 
