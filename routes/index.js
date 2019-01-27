6cee8941abf247bcb8ccc67481351c4b5112ca08
theblackathena <nicholaschin20@gmail.com>
2019-01-27 00:41:38 -0800

    login

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,48 @@ module.exports = {
         });
     },
     
+    getAddAccount: (req, res) => {
+        res.render('add-acc.ejs', {
+            title: "CripplingDebt | Create New Account",
+        });
+    },
+    
+    
+    addAccount: (req, res) => {
+        let username = req.body.username; 
+        let email = req.body.email; 
+        let password = req.body.pwd; 
+        let firstname = req.body.first_name; 
+        let lastname = req.body.last_name; 
+        let phone = req.body.phone; 
+        
+        let duplicateQuery = "SELECT * FROM 'account' WHERE username = '" + username + "'"; 
+        
+        db.query(duplicateQuery, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            if (result.length > 0) {
+                message = 'Username already exists';
+                res.render('create-acc.ejs', {
+                    message, 
+                    title: "CripplingDebt | Create New Account"
+                });
+            }
+            else {
+                let addUserQuery = "INSERT INTO 'account' (username, email, password, first_name, last_name, phone) VALUES ('" + username + "', '" + email + "', '" + password + "', '" + first_name + "', '" + last_name + "', '" + phone + "')";
+                
+                db.query(addUserQuery, (err, result) => {
+                    if (err) {
+                        return res.status(500).send(err);
+                    }
+                    res.redirect('/');
+                });
+            }
+        });
+    
+}
+        
+}
 
-    getACP: (req, res) => {
-        res.render('create-acc.ejs', {
-            title: "CripplingDebt | Create Account",            
-        })
-    }
-};
 
